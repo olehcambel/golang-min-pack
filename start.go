7d9706839fa970d4764ee618a90da36499a5cf0d
olehcambel <nickcambel5@gmail.com>
2018-08-11 03:18:19 +0300

    add webServer with redis-cli, build to exe

--- start.go
+++ start.go
@@ -4,15 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/mediocregopher/radix.v2/pubsub"
+	"github.com/mediocregopher/radix.v2/redis"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Web Server with GOlang")
+type service struct {
+	lastMessage  string
+	pubsubClient *pubsub.SubClient
+}
+
+func (s *service) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, s.lastMessage)
 	log.Println("/")
 }
 
+func (s *service) receive() {
+	for {
+		message := s.pubsubClient.Receive().Message
+		s.lastMessage = message
+		log.Println("New message", message)
+	}
+}
+
 func main() {
+	client, _ := redis.Dial("tcp", "localhost:6379")
+
+	var myService service
+	myService.pubsubClient = pubsub.NewSubClient(client)
+	myService.pubsubClient.Subscribe("subscribeChanngel")
+	go myService.receive()
+
 	log.Println("Server has started!")
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", myService.handler)
 	http.ListenAndServe(":6060", nil)
 }
